86e42b2b8e637cb2f54a73d482a4084640b3d2c5
Javad-mun <skhataeipour@mun.ca>
2019-08-23 14:46:30 -0230

    Updated NN.

--- Misc_codes/ApplyNN.R
+++ Misc_codes/ApplyNN.R
@@ -15,11 +15,25 @@ library(caret)
 library(stringr)
 library(MLmetrics)
 library(PRROC)
+library(data.table)
+library(tidyverse)
+library(lubridate)
+library(activityCounts)
+library(magrittr)
+library(scales)
+library(caret)
+library(stringr)
+library(MLmetrics)
+library(PRROC)
+library(gridExtra)
+library(kableExtra)
+library(imputeTS)
+library(Beap) # It's not on CRAN yet
 
 
 
 
-
+########################## Data preparation ###########################
 # ---------------------------- Read the data ----------------------
 
 # set the working directory to the folder that has the raw data and the intervals file
@@ -31,26 +45,127 @@ if (OS["sysname"] == "Windows") {
     main_path <-
         "/Volumes/hkr-storage/Research/dfuller/Walkabilly/studies/smarphone_accel/data"
 }
-setwd(dir = main_path)
 
+# Read the file with x,y,z axis and labels.
 Ethica_df <-
-    fread("Ethica_Jaeger_Merged/pocket/All_participants_pocket_with_count_for_Weka.csv")
+    fread(paste0(main_path,"/Ethica_Jaeger_Merged/pocket/All_participants_pocket_with_count_for_Weka.csv"))
 
 
+# ---------------------------- Prepare the data ----------------------------
 
+# Participants 112 , 121 .122 , and 132 are not properly classified
 working_df  <-
     Ethica_df %>% filter(!(participant_id %in% c(112, 121, 122, 132)))
 
-# Model A only uses raw accel data
-working_df   %<>%  dplyr::select(c(x_axis, y_axis, z_axis, trimmed_activity))
+# Model A_1 only uses raw accel data
+working_df %<>%  dplyr::select(c(x_axis, y_axis, z_axis, height, weight, age,gender, trimmed_activity))
+working_df  %<>% filter(trimmed_activity != "transit")
+
+# models canot work with strin
+working_df$trimmed_activity  %<>% str_replace_all(" ", "_") %>%
+    factor(
+        levels = c(
+            "Lying",
+            "Sitting",
+            "Self_Pace_walk",
+            "Running_3_METs",
+            "Running_5_METs",
+            "Running_7_METs"
+        ))
+
+
+
+frequency <- 30
+window_size_sec <- 2
+# The number of features reduces by this
+reduction_coef <-  window_size_sec *frequency 
+new_features_df <- GenerateFeatures(raw_df = working_df, window_size_sec = window_size_sec, frequency = frequency)
+
+
+# Increase the frequency
+new_features_df <- new_features_df[rep(1:nrow(new_features_df) , each = reduction_coef), ]
+
+
+# reduce the size of working_df to add new_features
+working_df  %<>% slice(1:nrow(new_features_df))
+## This model uses new_features too
+working_df <- bind_cols(working_df,new_features_df)
+# some models cannpt train if there is any NA
+working_df  %<>% na_interpolation(option =  "linear")
+
+
+# delete X, Y, Z axis
+working_df %<>%  dplyr::select(-c(x_axis, y_axis, z_axis))
+
+# To reduce the frequency
+## chose n = 60 
+working_df  %<>% groupdata2::group(n = 60, method = "greedy") %>%
+    rename("id" = .groups)
+working_df  %<>% group_by(id) %>%
+    summarise_all(first) %>%
+    select(-id)
+
+
+
+split_ratio = 0.7
+shrink = 1
+
+
+# Create train and test to train and evalute the model
+seed <- 2020
+set.seed(seed)
+
+working_df %<>% dplyr::sample_frac(shrink)
+message(paste0(shrink * 100, " % of the data will be used"))
+
+training_indices <-
+    createDataPartition(working_df$trimmed_activity,
+                        p = split_ratio,
+                        list = FALSE)
+
+# conver gender to number
+working_df$gender  %<>% as.factor() %>% as.integer()
+
+working_df$gender <- working_df$gender -1
+
+training_df <- working_df %>% dplyr::slice(training_indices)
+testing_df <- working_df %>% dplyr::slice(-training_indices)
+message(paste0("Data is devided into training and test set "))
+message(paste0(
+    "Training set has ",
+    nrow(training_df),
+    " rows and ",
+    ncol(training_df),
+    " columns"
+))
+message(paste0(
+    "Testing set has ",
+    nrow(testing_df),
+    " rows and ",
+    ncol(testing_df),
+    " columns"
+))
+
+
+
+
+
+
+######################## End of data preparation ##############################
 
 # models canot work with strin
 working_df$trimmed_activity  %<>% str_replace_all(" ", "_") %>%
     factor()
 
-working_df <-working_df %>% sample_frac(0.01)
-data <- working_df %>% select(-trimmed_activity) %>% as.matrix()
-labels <- working_df %>% select(trimmed_activity) %>% as.matrix() %>% as.factor() %>% as.integer()
+
+backup_df <- working_df
+
+
+
+
+data <- training_df %>% select(-trimmed_activity) %>% as.matrix()
+labels <- training_df %>% select(trimmed_activity) %>% as.matrix() %>% as.factor() %>% as.integer()
+
 
 labels <- labels - 1
 
@@ -60,7 +175,7 @@ opt <- optimizer_adam( clipvalue = 2.0)
 
 # add layers and compile the model
 model %>%
-    layer_dense(units = 100, activation = 'relu', input_shape = c(3)) %>%
+    layer_dense(units = 100, activation = 'relu', input_shape = c(59)) %>%
     layer_dense(units = 12, activation = 'relu') %>% 
     layer_dense(units = 6, activation = 'sigmoid') %>%
     compile(
@@ -79,5 +194,7 @@ one_hot_labels <- to_categorical(labels, num_classes = 6)
 
 
 # Train the model, iterating on the data in batches of 32 samples
-model %>% fit(data, one_hot_labels, epochs = 3, batch_size = 10)
+model %>% fit(data, one_hot_labels, epochs = 10, batch_size = 10)
 
+
+#keras::predict_classes()
