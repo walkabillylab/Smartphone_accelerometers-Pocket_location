3f2f882e4ae4bde8cd6f9984944a750c53aadfdf
Javad-mun <skhataeipour@mun.ca>
2019-09-13 15:01:12 -0230

    Initial commit. This file only  extract the Ethica data for each person. It doesn't change thee frequency

---
+++ Important_codes_prepare_data/1_Ethica_reader_extract_raw.R
@@ -0,0 +1,150 @@
+# Javad Khataei
+# Sept 10th, 2019
+
+# To extract raw data for each participamnts and save it is a csv file
+# This file does not change the frequency
+
+# import libraries
+library(data.table)
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+library(scales)
+library(signal)
+library(magrittr)
+
+
+## -------------------------------------The root folder---------------------------------##
+# set the working directory to the folder that has the raw data and the intervals file
+OS <- Sys.info()
+if (OS["sysname"] == "Windows") {
+  main_path <-
+    "Z:/Research/dfuller/Walkabilly/studies/smarphone_accel/data/Ethica"
+} else {
+  main_path <-
+    "/Volumes/hkr-storage/Research/dfuller/Walkabilly/studies/smarphone_accel/data/Ethica"
+}
+setwd(dir = main_path)
+
+
+
+## ------------------------- loop through all months and locations---------------------##
+mons <- c("01", "02", "03", "04", "05")
+
+dev_ids <- c("2673", "2674", "2675")
+# #set the study year
+study_year <- "2019"
+
+
+for (dev_id in dev_ids) {
+  if (dev_id == "2673") {
+    wear_loc <- "hand"
+  }
+  if (dev_id == "2674") {
+    wear_loc <- "back"
+  }
+  if (dev_id == "2675") {
+    wear_loc <- "pock"
+  }
+
+
+  for (mon in mons) {
+
+    # create study's year and month
+    study_year_month <- paste0(study_year, "_", mon)
+
+    # create the file name to read the data
+    file_name <-
+      paste0(dev_id, "_", study_year, "_", mon, "_accelerometer.csv")
+
+
+    # read the accelerameter file
+    df <- fread(file = file_name)
+    df$record_time <- df$record_time %>% as_datetime()
+
+
+    # read the interval files that has all the intrvals for different participants
+    intervals_file <- fread(file = "intervals.csv") 
+    
+
+
+    # get the year and the month out of intervals_file
+    intervals_file <-
+      as.Date(intervals_file$start) %>%
+      format(format = "%Y_%m") %>%
+      as.character() %>%
+      mutate(intervals_file, year_month = .) %>%
+      na.omit()
+
+    # Select participants who participated during study_year_month
+    Selected_part <-
+      intervals_file %>% dplyr::filter(intervals_file$year_month == study_year_month)
+
+
+    # for each selected participant
+    Selected_part$userid %>% map(function(i) {
+
+      # get the current participant and extract its info,i.e id, start and end time
+      current_participant <-
+        Selected_part %>% dplyr::filter(Selected_part$userid == i)
+
+      participant_id <- current_participant$userid
+
+      participant_start_time <-
+        current_participant$start %>% as_datetime()
+
+      participant_end_time <- current_participant$end %>% as_datetime()
+
+
+      # adjust the raw data time using a temporary var
+      daylight_saving <- "2019-03-10"
+
+
+      # Participants after daylight saving
+      temp_df <- df
+      if (participant_start_time > daylight_saving) {
+        if (dev_id == "2673") {
+          temp_df$record_time <- temp_df$record_time - hms("2:32:00")
+        } else if (dev_id == "2674") {
+          temp_df$record_time <- temp_df$record_time - hms("2:38:00")
+        } else if (dev_id == "2675") {
+          temp_df$record_time <- temp_df$record_time - hms("2:36:00")
+        }
+      } else {
+        # Before daylight saving
+        if (dev_id == "2673") {
+          temp_df$record_time <- temp_df$record_time - hms("3:32:00")
+        } else if (dev_id == "2674") {
+          temp_df$record_time <- temp_df$record_time - hms("3:38:00")
+        } else if (dev_id == "2675") {
+          temp_df$record_time <- temp_df$record_time - hms("3:36:00")
+        }
+      }
+
+      # find the start and the end of the participant's data
+      record_time <-
+        temp_df$record_time %>%
+        as_datetime() %>%
+        floor_date(unit = "second")
+      start_time_index <-
+        which(record_time > participant_start_time) %>% first()
+      end_time_index <-
+        which(record_time < participant_end_time) %>% last()
+
+
+      # slice the participant's part
+      df_slice <- temp_df %>% slice(start_time_index:end_time_index)
+
+
+      # add participant's id and waer location
+      df_slice <-
+        df_slice %>% mutate(participant_id = participant_id, wear_location = wear_loc)
+      p_id <- as.character(participant_id)
+      dir.create(p_id, showWarnings = FALSE)
+      setwd(paste0(p_id, "/"))
+
+      fwrite(x = df_slice, file = paste0(p_id, "_", wear_loc, "_raw.csv"))
+      setwd(dir = main_path)
+    })
+  }
+}
